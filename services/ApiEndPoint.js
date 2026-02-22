e4a0251976f32567ca1fadae8573ccab142c2714
maazulhaque26-ship-it <maazulhaque26@gmail.com>
2026-02-22 13:55:11 +0530

    modified apiEndpoint

--- services/ApiEndPoint.js
+++ services/ApiEndPoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://notes-backend-21a9.onrender.com',
     headers :{
         "content-type" :"application/json"
     }
